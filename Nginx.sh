f96f2f744fd9615264bd657ba3765d11c58660f7
AEprogs <160319650+AEprogs@users.noreply.github.com>
2024-05-29 20:05:48 +0800

    Update Nginx.sh

--- Nginx.sh
+++ Nginx.sh
@@ -7,5 +7,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is $(AaronEnriquez).</h2></body></html>" | sudo tee -a /var/www/html/index.html
-
+echo "<html><body><h2>Welcome to Azure! My name is $(AaronEnriquez).</h2></body></html>" 
